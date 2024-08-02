a81dc47be59f8b5b7d7b88a084ea6eb735723e4c
Priyanshu-200 <135335930+Priyanshu-200@users.noreply.github.com>
2024-08-02 07:00:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config({path: './config/config.env'});
 
 app.use(cors({
-    origin: [process.evn.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL],
     methods: ['GET','POST','DELETE','PUT'],
     credentials: true
 }));
